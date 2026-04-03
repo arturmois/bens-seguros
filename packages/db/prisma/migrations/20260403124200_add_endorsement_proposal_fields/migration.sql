085a0b3d3dc5e4402cb2438b172c6ce99e10885f
Artur Moises <arturmoiscontato@gmail.com>
2026-04-03 12:53:19 -0300

    fix(db): align endorsement migration indexes

--- packages/db/prisma/migrations/20260403124200_add_endorsement_proposal_fields/migration.sql
+++ packages/db/prisma/migrations/20260403124200_add_endorsement_proposal_fields/migration.sql
@@ -9,6 +9,9 @@ ALTER TABLE "Proposal"
 CREATE INDEX "Proposal_organizationId_sourcePolicyId_idx"
   ON "Proposal" ("organizationId", "sourcePolicyId");
 
+CREATE INDEX "Proposal_organizationId_boardType_createdAt_idx"
+  ON "Proposal" ("organizationId", "boardType", "createdAt" DESC);
+
 ALTER TABLE "Proposal"
   ADD CONSTRAINT "Proposal_sourcePolicyId_fkey"
   FOREIGN KEY ("sourcePolicyId") REFERENCES "Policy"("id")
